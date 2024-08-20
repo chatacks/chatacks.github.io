9a47583a63ab62cbf63f8f1648bcb1e3ff3179fc
Thyago <chatack.tc@gmail.com>
2024-08-20 17:59:02 -0300

    :sparkles: [feat]: add useEffect em componente Infos para resgatar os dados da api do github e atualiza currículo para botao de download cv

--- src/components/About/Infos.tsx
+++ src/components/About/Infos.tsx
@@ -1,4 +1,31 @@
+/* eslint-disable prefer-const */
+import { useEffect, useState } from 'react';
+import getGithubRepositories from '../../services/getGithubRepositories';
+
 function Infos() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchEffect = async () => {
+      try {
+        const repositories = await getGithubRepositories();
+        if (isMounted) {
+          setData(repositories);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchEffect();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
   return (
     <div className="about-info grid">
       <div className="about-box">
@@ -10,7 +37,7 @@ function Infos() {
       <div className="about-box">
         <i className="bx bx-briefcase-alt about-icon" />
         <h3 className="about-title">Projetos</h3>
-        <span className="about-subtitle">4 Projetos +</span>
+        <span className="about-subtitle">{`${data.length} Projetos Feitos`}</span>
       </div>
 
     </div>
