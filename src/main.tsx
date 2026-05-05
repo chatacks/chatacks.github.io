8c0bd6eabd41d6e70884c2e70e71d031eee8bf06
Thyago Chatack <chatack.tc@gmail.com>
2026-05-04 21:31:10 -0300

    :sparkles: [feat]: add ModeToggle component for theme switching and integrate it into the Navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { LanguageProvider } from "./i18n/LanguageContext";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as Element).render(
+ReactDOM.createRoot(document.getElementById("root") as Element).render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <RouterProvider router={router} />
+    </LanguageProvider>
   </React.StrictMode>,
 );
